2674a357130f55d7c9de6817a2cdf27c5070df3a
Gabriel Silva <gabrielkko7@gmail.com>
2022-12-23 15:57:45 -0300

    1 commit

---
+++ js/scriptSemaforo.js
@@ -0,0 +1,37 @@
+//js
+
+const img = document.getElementById("sem");
+const buttons = document.getElementById("buttons");
+let colorIndex = 0;
+let intervalID = null;
+
+const trafficLight = (event) =>
+{
+    stopAutomatic();
+    turnOn[event.target.id]();
+}
+
+const nextIndex = () => colorIndex = colorIndex < 2? ++colorIndex:0;
+
+const changeColor = () => 
+{
+    const colors = ['red','yellow','green'];
+    const color = colors[colorIndex];
+    turnOn[color]();
+    nextIndex();
+}
+
+const stopAutomatic = () => 
+{
+    clearInterval(intervalID);
+}
+
+const turnOn =
+{
+    'red' : () => img.src='img/vermelho.png',
+    'yellow' : () => img.src='img/amarelo.png',
+    'green' : () => img.src='img/verde.png',
+    'automatic' : () => intervalID=setInterval(changeColor,1000)
+}
+
+buttons.addEventListener('click',trafficLight);
